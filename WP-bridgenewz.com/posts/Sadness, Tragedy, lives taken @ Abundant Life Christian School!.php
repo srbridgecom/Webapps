30ca68f7c922dc3085ee184b795c95d262f5eda7
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2024-12-17 08:47:20 -0800

    Create Sadness, Tragedy, lives taken @ Abundant Life Christian School!.php

---
+++ WP-bridgenewz.com/posts/Sadness, Tragedy, lives taken @ Abundant Life Christian School!.php
@@ -0,0 +1,83 @@
+Sadness, Tragedy, lives taken @ Abundant Life Christian School!
+
+Thoughts and condolences to the family's involved and affected. This should never have happened.
+
+<strong>THERE'S HELP OUT THERE FOR ANYONE WHO NEEDS IT! MESSAGE ANYONE, ask authorities for help!</strong>
+
+<strong>DIAL OR TEXT 998!</strong>
+
+<strong>OR DIAL 800-273-TALK (8255)</strong>
+
+<img class="rounded-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[300px] w-[570px] object-cover" src="https://www.nami.org/wp-content/uploads/2023/04/who-we-are-intro.jpg" alt="" />
+
+<strong>National Suicide Hotline</strong>
+
+&nbsp;
+
+MADISON, Wis. — A teacher and a teenage student are dead following a tragic shooting at Abundant Life Christian School in Madison. Authorities confirmed the 15-year-old female shooter, a student at the school, died by suicide.
+
+[caption id="attachment_126" align="aligncenter" width="538"]<img class="size-full wp-image-126" src="https://bridgenewz.com/wp-content/uploads/2024/12/AbundantSchool-2024-12-16-18-57-31.webp" alt="AbundantSchool 2024-12-16 18-57-31" width="538" height="272" /> AbundantSchool 2024-12-16 18-57-31,[/caption]
+
+&nbsp;
+
+The incident occurred during a study hall with students from various grades present. Madison police outlined the timeline during an evening briefing:
+<ul>
+ 	<li><strong>9:57 a.m.</strong>: A second-grader called 911.</li>
+ 	<li><strong>11:00 a.m.</strong>: The first Madison deputy arrived on the scene.</li>
+ 	<li><strong>11:05 a.m.</strong>: Madison Fire Department arrived.</li>
+ 	<li><strong>11:13 a.m.</strong>: Officers began clearing the school.</li>
+</ul>
+Six others were injured in the shooting, including one teacher and three students with non-life-threatening injuries. Two victims remain hospitalized in critical condition, while two others were discharged.
+
+Students have been safely reunited with their families. The school posted on Facebook expressing gratitude for the overwhelming support: “We are grateful for the tsunami of prayers for our students, staff, and families.”
+
+Barbara Weirs, Director of Elementary and School Relations, revealed that the school does not have metal detectors but follows safety protocols, including a lockdown drill earlier this year.
+
+Witnesses recounted the chaotic scene. A young student described hearing gunshots while preparing for lunch. Nearby resident Guy Nelson said, “It’s very scary. You always see it on TV, but when it’s in your backyard, it’s shocking.”
+
+Police confirmed the shooter’s home was searched, but further details were not disclosed. Roads near the school remain blocked as investigations continue.
+
+The Madison Metropolitan School District placed nine schools on secure hold, lifted at 1:15 p.m. Despite the tragedy, MMSD announced that classes would continue as scheduled on Tuesday.
+
+Authorities ask the community for continued thoughts and prayers as they work through this devastating event.
+
+&nbsp;
+
+Gov. Tony Evers released the following statement on the shooting:
+
+<strong><em>I am closely monitoring the incident at Abundant Life Christian School in Madison. We are praying for the kids, educators, and entire Abundant Life school community as we await more information and are grateful for the first responders who are working quickly to respond.</em></strong>
+
+Evers ordered flags to be flown at half-staff immediately until sunset on Sunday out of respect for the victims of the shooting.
+
+He issued a second statement Monday afternoon:
+
+<strong><em>There are no words to describe the devastation and heartbreak we feel today after the school shooting at Abundant Life Christian School in Madison this morning.</em></strong>
+
+<strong><em>Kathy and I join the people of Wisconsin in praying for the families and loved ones of those whose lives were so senselessly taken and for the educators, staff, and the entire Abundant Life school community as they grapple with the grief, trauma, and loss of this gut-wrenching tragedy. We are also praying and hoping all those injured survive and recover.  </em></strong>
+
+<strong><em>Kathy and I are also keeping in our hearts the first responders who acted quickly and ran toward danger to help save lives. This day will no doubt weigh heavily on them, and we are grateful for their critical response efforts in answering the call when they were needed most.</em></strong>
+
+<strong><em>As a father, a grandfather, and as governor, it is unthinkable that a kid or an educator might wake up and go to school one morning and never come home. This should never happen, and I will never accept this as a foregone reality or stop working to change it. </em></strong>
+
+<strong><em>Today my focus is on supporting these families and kids and the Abundant Life community, and the state stands ready to support them and the efforts of local law enforcement through what will undoubtedly be difficult days ahead.</em></strong>
+
+Dane County Executive Melissa Agard also issued a statement:
+
+<strong><em>Today, our community is reeling from the heartbreaking and senseless tragedy at Abundant Life Christian School in Madison. My heart goes out to the victims, their families, the students, educators, and everyone affected by this unimaginable and unacceptable violence.</em></strong>
+<p class="x_MsoNormal"><strong><em>Dane County stands united in grief with the Abundant Life Christian School community. We are committed to supporting the school, its students, and their families during this devastating time, and will work closely with law enforcement and other first responders, mental health professionals, and community leaders to provide resources and care for those who need it most.</em></strong></p>
+<p class="x_MsoNormal"><strong><em>No community should ever have to endure such a tragedy. My administration will do everything in its power to provide assistance in the face of this devastating loss. To all those grieving, please know that you are not alone. Dane County stands with you, and we are here to support you in any way possible. I have been in communication with the Governor’s office as well as the Mayor of Madison’s office and we will continue to update the public as we learn more.</em></strong></p>
+&nbsp;
+
+<strong>THERE'S HELP OUT THERE FOR ANYONE WHO NEEDS IT! MESSAGE ANYONE, ask authorities for help!</strong>
+
+<strong>DIAL OR TEXT 998!</strong>
+
+<strong>OR DIAL 800-273-TALK (8255)</strong>
+
+<strong>National Suicide Hotline</strong>
+
+<img class="rounded-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[300px] w-[570px] object-cover" src="https://www.nami.org/wp-content/uploads/2023/04/who-we-are-intro.jpg" alt="" />
+
+&nbsp;
+
+Author: Ryan Bridglal, 12/16/2024
