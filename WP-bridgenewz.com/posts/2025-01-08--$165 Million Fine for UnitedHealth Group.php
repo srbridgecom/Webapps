442611cc24c2ffcef5b1bcb8c37e53dfef475a2e
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2025-01-08 18:39:38 -0800

    Create 2025-01-08--$165 Million Fine for UnitedHealth Group.php

---
+++ WP-bridgenewz.com/posts/2025-01-08--$165 Million Fine for UnitedHealth Group.php
@@ -0,0 +1,35 @@
+The LARGEST medical insurance company to date; $50 Million paid out to Consumers, $115 million in civil penalties as ordered by the office above the Massachusetts Consumer Protection Act. Court Order Link Included!
+
+I believe the $115 Million should be directed towards Consumers, but hey, it’s some kind of win! HealthMarkets Inc, The Chesapeake Life Insurance Company and HealthMarkets Insurance Agency VIOLATED regulations in Massachusetts by MISLEADING consumers into buying additional health insurance; along with misrepresenting agents as neutral parties.
+<div class="ojp-nav__logo--default" title="UnitedHealth Group logo" data-svg="uhg-logo--desktop-2022">
+
+[caption id="attachment_197" align="aligncenter" width="493"]<img class="size-full wp-image-197" src="https://bridgenewz.com/wp-content/uploads/2025/01/unitedhealth-mini2025-01-08-18-18-38.webp" alt="unitedhealth mini2025-01-08 18-18-38" width="493" height="454" /> UnitedHealthGroup HQ as obtained &amp; Quoted  from Google. 01-08-2025[/caption]
+
+</div>
+&nbsp;
+
+COMMONWEALTH OF MASSACHUSETTS COURT ORDER LINK:
+
+<a href="https://www.mass.gov/doc/superior-court-order-healthmarkets-inc-december-2024/download">https://www.mass.gov/doc/superior-court-order-healthmarkets-inc-december-2024/download</a>
+
+A Massachusetts court has ordered three health insurance companies to pay $165 million in restitution and civil penalties after finding them liable for widespread deceptive practices. According to a 48-page court order, the companies’ advertisements—aired on television, radio, and the internet—misrepresented their services as free and impartial, while they were neither. The court also found that their agents falsely claimed to be “impartial,” “objective,” and “unbiased,” both in advertisements and in direct conversations with potential customers, despite their primary goal being to sell Chesapeake supplemental health insurance products.
+
+The companies further misled consumers by “bundling” supplemental health insurance with major medical insurance and falsely describing the bundled plans as “coverage” while stating they were “not insurance and do not guarantee the payment of health insurance costs.”
+
+“Notably, in light of evidence that the defendants intentionally targeted ‘vulnerable consumers who could least afford their products,’ the Court found that the companies’ deceptive conduct was ‘particularly egregious,’” stated the Massachusetts Attorney General’s office.
+
+The legal proceedings began in December 2020 when then-Attorney General Maura Healey filed a complaint alleging the companies had violated Massachusetts consumer protection laws. Healey accused the companies of misleading more than 15,000 Massachusetts residents into purchasing unnecessary health insurance products, resulting in $43.5 million in consumer losses since 2011.
+
+In April 2022, the Superior Court found the companies liable. After a trial on remedies, the court issued its findings and ruling on December 31, 2024. “The Court found that the defendants engaged in widespread misrepresentations of supplemental health insurance that they sold in Massachusetts, including intentionally engaging in bundling of major medical and supplemental insurance to deceive consumers into unknowingly buying supplemental policies,” the AG’s office noted.
+
+UnitedHealthcare, which acquired the three companies in 2019, plans to appeal the decision. “We disagree with the Massachusetts court’s latest ruling in the litigation involving the HealthMarkets companies,” from a UnitedHealthcare spokesperson. “The fundamental errors in this ruling compound those already made by the trial court earlier in this case and have resulted in a decision that is clearly unsupported by the evidence and contrary to established Massachusetts law.”
+
+The court order requires $50 million of the $165 million to go toward reimbursing consumers who paid for the misleading policies. The remaining $115 million constitutes civil penalties, reflecting the “egregiousness of respective violations.”
+
+UnitedHealthcare has emphasized that the deceptive practices occurred prior to its acquisition of the companies. Nevertheless, the healthcare industry, including UnitedHealth Group, has faced heightened scrutiny in recent months. This comes after UnitedHealth Group’s CEO was shot and killed last month in a premeditated attack by Luigi Mangione, drawing further attention to the company. UnitedHealth Group remains the largest medical insurance provider in the United States.
+
+Information obtained from the Massachusetts Attorney General’s Office Press Release, 01/08/2025
+
+Author: Ryan Bridglal, 01/08/2025
+<div class="ojp-nav__logo--stacked" title="UnitedHealth Group logo" data-svg="uhg-logo--stacked-2022"></div>
+<a class="ojp-nav__logo-link" tabindex="0" href="https://www.unitedhealthgroup.com/content/uhg/en.html"><!-- End UHG Stacked Logo --> </a>
