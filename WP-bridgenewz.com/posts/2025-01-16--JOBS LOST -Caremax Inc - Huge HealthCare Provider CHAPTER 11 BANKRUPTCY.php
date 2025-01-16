a5eb349424f6d791004e8b1dadacc72adfe5a0b3
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2025-01-16 09:54:43 -0800

    Create 2025-01-16--JOBS LOST -Caremax Inc - Huge HealthCare Provider CHAPTER 11 BANKRUPTCY.php

---
+++ WP-bridgenewz.com/posts/2025-01-16--JOBS LOST -Caremax Inc - Huge HealthCare Provider CHAPTER 11 BANKRUPTCY.php
@@ -0,0 +1,31 @@
+This is a huge BLOW to the economy/job market in Texas, Tennessee, New York and Florida…. They ARE/WERE HQ in Miami, Florida! When the HQ goes under, it’s done!
+<ul>
+ 	<li>JOBS LOST!</li>
+ 	<li>Stock price was $488 – $490 a share on January 2021 to $.0010 a share today, January 16<sup>th</sup> 2025!</li>
+ 	<li>Estimated 1450 to 2000 jobs lost as the company goes out of business.</li>
+ 	<li>50 to 60 medical centers</li>
+ 	<li>11/20/2024 – filed for bankruptcy in Texas</li>
+</ul>
+[caption id="attachment_217" align="aligncenter" width="981"]<img class="size-full wp-image-217" src="https://bridgenewz.com/wp-content/uploads/2025/01/caremax-2025-01-16-09-28-19.webp" alt="caremax 2025-01-16 09-28-19" width="981" height="389" /> caremax 2025-01-16 09-28-19, Referenced-Quoted, Caremax.com[/caption]
+
+&nbsp;
+
+CareMax, Inc., a Miami-based healthcare firm, has filed for bankruptcy, listing debts of $693 million against assets of $390 million in a filing with the U.S. Bankruptcy Court for the Northern District of Texas. In August, the company reported a second-quarter loss of $170.6 million and issued a going-concern warning. Earlier this month, CareMax disclosed its inability to file its third-quarter report with the U.S. Securities and Exchange Commission due to insufficient funds.
+
+The company intends to pursue the sale or other transactions for its management services organization and core center assets. The board has approved hiring Alvarez &amp; Marsal as financial advisers and Piper Sandler as investment banker.
+
+This development follows the bankruptcy of other healthcare groups this year, including Massachusetts-based Steward Health Care, which filed for bankruptcy in May, seeking to sell all 31 hospitals to address $9 billion in debt. Notably, in late 2022, CareMax acquired the Medicare value-based business of Steward for $25 million in cash and 23.5 million shares of its stock. CareMax shares closed at $1.68 on Friday, down 89% year-to-date.
+
+Despite these financial challenges, CareMax has demonstrated a commitment to community impact. In its 2022 Impact Report, the company highlighted its "Whole Person Health" approach, emphasizing prevention, healthy behaviors, improved access to specialists, and chronic disease management. In 2022, CareMax patients experienced a 69% reduction in emergency room visits per thousand and a 46% reduction in hospital admissions per thousand compared to the national average. Additionally, CareMax achieved a Centers for Medicare and Medicaid Services (CMS) 5-Star Rating across all clinical and pharmacy measures.
+
+Furthermore, CareMax engaged with communities by building strong alliances with government agencies and other organizations. Working with community partners, the company raised more than $1.5 million in resources, including food, water, and supplies, which were delivered to Florida areas hardest hit by Hurricane Ian.
+
+As CareMax navigates its financial restructuring, the company's recent impact report underscores its ongoing efforts to improve healthcare outcomes and support underserved populations.
+
+Sources:
+caremax.com
+https://www.caremax.com/2023/06/08/caremax-releases-2022-impact-report/
+
+Author: Ryan Bridglal, 01/16/2025
+
+&nbsp;
