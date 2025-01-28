ee18c441551c9366a50eb70fc74a615a80ddcfe6
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2025-01-28 08:52:22 -0800

    Create 2025-01-28--Chick Fil A– Offering Free Chicken Sandwiches LAS VEGAS AREA.php

---
+++ WP-bridgenewz.com/posts/2025-01-28--Chick Fil A– Offering Free Chicken Sandwiches LAS VEGAS AREA.php
@@ -0,0 +1,28 @@
+THIS IS AWESOME, especially for the homeless. Certain ChickFilA locations in Las Vegas is giving away free chicken sandwiches.
+
+<img class="desktop-img aligncenter" src="https://d1fd34dzzl09j.cloudfront.net/Images/CFACOM/Daypart%20Hero/Winter23/Winter23-DotCom-ChickenSandwich-D_720x748.jpg?la=en&amp;h=748&amp;w=720" alt="" /><em>Quoted, referenced from https://www.chick-fil-a.com/, 01/28/2025</em>
+
+&nbsp;
+
+<strong>HOW TO OBTAIN?</strong>
+
+From Monday, Jan. 27, through Saturday, Feb. 1, guests can enjoy one free Original Chick-fil-A Chicken Sandwich, Spicy Chicken Sandwich, or Grilled Chicken Sandwich.
+
+To claim the offer, guests need to:
+<ul>
+ 	<li>Download the Chick-fil-A app</li>
+ 	<li>Log in or create an account</li>
+ 	<li>Choose a participating restaurant</li>
+ 	<li>Redeem the offer</li>
+</ul>
+The promotion is available through the app while supplies last and is limited to one sandwich per person, per Chick-fil-A account.
+
+&nbsp;
+
+<strong>Sources:</strong>
+
+https://www.chick-fil-a.com/
+
+&nbsp;
+
+Author: Ryan Bridglal, 01/28/2025
