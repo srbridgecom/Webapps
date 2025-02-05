ecbe5be5ee16210958347033fb0b19fdd54579ba
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2025-02-05 12:30:38 -0800

    Create 2025-02-05--USAID 10k Jobs-Why-COST = DOGE-NOT Coin-Dept. of Gov. Efficiency.php

---
+++ WP-bridgenewz.com/posts/2025-02-05--USAID 10k Jobs-Why-COST = DOGE-NOT Coin-Dept. of Gov. Efficiency.php
@@ -0,0 +1,111 @@
+Money bled 1961-2025
+
+&nbsp;
+
+1.2 Bn UNDISCLOSED RECIPIENTS ← WTF? I wasn't included!
+$520 Mn on ESG
+2023, 40 Bn
+2024, 44 Bn
+Need I go on?
+
+10k Jobs on leave/Cut
+130 Countries…. Yes! 130!
+
+“Live by executive order, die by executive order,” Musk said on X Saturday in reference to USAID.
+<ul>
+ 	<li>A March 2023 AP-NORC poll found that 6 in 10 Americans believed the U.S. was spending too much on foreign aid.</li>
+ 	<li>Roughly 7 in 10 said the government was spending too much on assistance to other countries</li>
+ 	<li>90% of Republicans and 55% of Democrats agreed that foreign aid spending was excessive.</li>
+</ul>
+[caption id="attachment_255" align="aligncenter" width="1253"]<img class="size-full wp-image-255" src="https://bridgenewz.com/wp-content/uploads/2025/02/USAID2025-02-05-11-52-25.webp" alt="USAID2025-02-05 11-52-25" width="1253" height="348" /> USAID2025-02-05 11-52-25, as quoted see link below[/caption]
+<h3 class="western"><span style="color: #434343;"><span style="font-family: Arial, sans-serif;"><span style="font-size: large;"><b>What is USAID?</b></span></span></span></h3>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">The United States Agency for International Development (USAID) was established by President John F. Kennedy in 1961 at the height of the Cold War. He sought a more effective way to counter Soviet influence abroad through foreign assistance, believing that the State Department’s bureaucratic inefficiencies hindered these efforts. Congress responded by passing the Foreign Assistance Act, officially creating USAID as an independent agency.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Although the Soviet Union collapsed in 1991, USAID has continued to operate, with supporters arguing that U.S. foreign assistance serves as a counterbalance to growing Chinese and Russian influence. China, for example, has expanded its Belt and Road Initiative, a global foreign aid and infrastructure program that competes for influence in many of the same countries where the U.S. seeks partnerships.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">However, critics argue that USAID programs are wasteful and promote a liberal agenda.</span></span></span>
+<h3 class="western"></h3>
+<h3 class="western"><span style="color: #434343;"><span style="font-family: Arial, sans-serif;"><span style="font-size: large;"><b>What’s Happening with USAID?</b></span></span></span></h3>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">On January 20, his first day back in office, President Trump implemented a 90-day freeze on foreign assistance programs. Just four days later, Peter Marocco, a returning political appointee from Trump’s first term, drafted an unexpectedly strict interpretation of the order, effectively shutting down thousands of aid programs worldwide and leading to mass furloughs and layoffs.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Secretary of State Marco Rubio has since stepped in to allow certain life-saving emergency programs to continue during the freeze. However, uncertainty surrounding exemptions and growing fears of permanent funding cuts have further disrupted USAID’s operations.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">In addition to the furloughs, USAID employees were reportedly instructed not to enter the agency’s Washington, D.C. headquarters, and its website and official X (formerly Twitter) account have been taken down.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">This move is part of a broader Trump administration effort to reshape the federal government, with USAID being among the hardest-hit agencies. According to Rubio, the administration intends to conduct a program-by-program review to determine which projects “make America safer, stronger, or more prosperous.”</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Shutting down aid programs during the 90-day review has, according to Rubio, already resulted in greater “cooperation” from countries receiving U.S. humanitarian, development, and security assistance.</span></span></span>
+<h3 class="western"></h3>
+<h3 class="western"><span style="color: #434343;"><span style="font-family: Arial, sans-serif;"><span style="font-size: large;"><b>Criticism of USAID</b></span></span></span></h3>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Debates over USAID funding generally follow party lines.</span></span></span>
+<ul>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Republicans often push for the State Department—which oversees foreign policy—to have greater control over USAID’s policies and budget.</span></span></span></li>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Democrats advocate for USAID to remain autonomous, arguing that it is a vital tool of U.S. diplomacy.</span></span></span></li>
+</ul>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Historically, Republican administrations have sought to cut funding for United Nations programs, including those related to peacekeeping, human rights, and refugee support.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">During Trump’s first term, the administration reduced foreign aid spending and suspended payments to various UN agencies, including:</span></span></span>
+<ul>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">The UN Population Fund</span></span></span></li>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">The Palestinian Authority</span></span></span></li>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">The UN Human Rights Council (the U.S. also withdrew entirely)</span></span></span></li>
+</ul>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">In 2023, President Biden signed a bill barring U.S. funding for the UN agency for Palestinian refugees (UNRWA).</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Despite generally supporting foreign aid, Rubio has consistently pushed for greater transparency in spending. In 2017, he described foreign assistance as “not charity” but something that must be “well spent” because it is “critical to our national security.”</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">In 2023, he sponsored legislation requiring U.S. foreign aid agencies to disclose which organizations were implementing aid programs on the ground.</span></span></span>
+<h3 class="western"></h3>
+<h3 class="western"><span style="color: #434343;"><span style="font-family: Arial, sans-serif;"><span style="font-size: large;"><b>Why is Elon Musk Targeting USAID?</b></span></span></span></h3>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Elon Musk’s Department of Government Efficiency (DOGE)—a new federal office empowered by Trump—has spearheaded efforts to fire government workers and cut trillions in spending.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">USAID has become one of DOGE’s primary targets. Musk has publicly accused USAID of funding “deadly programs” and labeled the agency a “criminal organization.”</span></span></span>
+<h3 class="western"></h3>
+<h3 class="western"><span style="color: #434343;"><span style="font-family: Arial, sans-serif;"><span style="font-size: large;"><b>Impact of the USAID Freeze</b></span></span></span></h3>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">The foreign aid freeze has hit Sub-Saharan Africa particularly hard. Last year, the U.S. provided the region with over $6.5 billion in humanitarian assistance. Now, some HIV/AIDS clinics funded by the U.S. President’s Emergency Plan for AIDS Relief (PEPFAR)—an initiative that helped combat the global AIDS epidemic of the 1980s—have been forced to close.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">The freeze is also disrupting aid efforts in Latin America:</span></span></span>
+<ul>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Mexico: A migrant shelter has been left without a doctor.</span></span></span></li>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Venezuela: A mental health program for LGBTQ+ youth has been disbanded.</span></span></span></li>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Colombia, Costa Rica, Ecuador, and Guatemala: Safe Mobility Offices, where migrants could apply for legal entry into the U.S., have been shuttered.</span></span></span></li>
+</ul>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Aid organizations are struggling to assess the full impact of the freeze, including the number of programs shut down and workers furloughed or laid off.</span></span></span>
+<h3 class="western"></h3>
+<h3 class="western"><span style="color: #434343;"><span style="font-family: Arial, sans-serif;"><span style="font-size: large;"><b>How Much Does the U.S. Spend on Foreign Aid?</b></span></span></span></h3>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">In fiscal year 2023, the U.S. spent approximately $40 billion on foreign aid, according to a Congressional Research Service report published last month.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">While the U.S. is the largest global provider of humanitarian assistance, some countries spend a larger percentage of their national budget on foreign aid. Overall, U.S. foreign aid accounts for less than 1% of the total federal budget.</span></span></span>
+<h3 class="western"></h3>
+<h3 class="western"><span style="color: #434343;"><span style="font-family: Arial, sans-serif;"><span style="font-size: large;"><b>What Do Americans Think About Foreign Aid?</b></span></span></span></h3>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Public opinion on foreign aid is overwhelmingly skeptical:</span></span></span>
+<ul>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">A March 2023 AP-NORC poll found that 6 in 10 Americans believed the U.S. was spending too much on foreign aid.</span></span></span></li>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Roughly 7 in 10 said the government was spending too much on assistance to other countries.</span></span></span></li>
+ 	<li><span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">90% of Republicans and 55% of Democrats agreed that foreign aid spending was excessive.</span></span></span></li>
+</ul>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Interestingly, Americans overestimate how much the U.S. spends on foreign aid. Surveys by the Kaiser Family Foundation found that, on average, Americans believe foreign aid accounts for 31% of the federal budget—a figure far higher than the actual 1% or less.</span></span></span>
+<h3 class="western"></h3>
+<h3 class="western"><span style="color: #434343;"><span style="font-family: Arial, sans-serif;"><span style="font-size: large;"><b>Could Trump Eliminate USAID?</b></span></span></span></h3>
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">Democrats argue that presidents lack the constitutional authority to unilaterally dissolve USAID. However, it remains unclear what legal challenges could prevent Trump from attempting it.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">A similar legal battle played out during Trump’s first term, when he sought to slash the foreign operations budget by one-third. When Congress rejected these cuts, the Trump administration used funding freezes and other tactics to limit aid disbursement.</span></span></span>
+
+<span style="color: #000000;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;">The Government Accountability Office later ruled that these actions violated the Impoundment Control Act, which prohibits the executive branch from withholding congressionally approved funds.</span></span></span>
+
+&nbsp;
+
+&nbsp;
+
+Sources:
+
+<a href="https://www.state.gov/policy-issues/u-s-agency-for-international-development-usaid/"><span style="color: #1155cc;"><span style="font-family: Arial, sans-serif;"><span style="font-size: small;"><u>https://www.state.gov/policy-issues/u-s-agency-for-international-development-usaid/</u></span></span></span></a>
+
+Featured Image: <a class="userName--owby3" href="https://pixabay.com/users/barta4-31077095/" target="_blank" rel="noopener">Barta4</a>
+
+https://pixabay.com/photos/money-cash-dollars-finance-7881948/
+<div></div>
+<div></div>
+Author: Ryan Bridglal, 02/05/2025
+
+&nbsp;
