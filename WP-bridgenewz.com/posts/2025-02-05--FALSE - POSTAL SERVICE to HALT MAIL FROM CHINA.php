e01937b25aef4c10816d5ccf3fdd0083c56054b9
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2025-02-05 09:53:57 -0800

    Create 2025-02-05--FALSE - POSTAL SERVICE to HALT MAIL FROM CHINA.php

---
+++ WP-bridgenewz.com/posts/2025-02-05--FALSE - POSTAL SERVICE to HALT MAIL FROM CHINA.php
@@ -0,0 +1,23 @@
+FALSE - POSTAL SERVICE to HALT MAIL FROM CHINA
+
+Shop local first, ensure it’s shipped from a nationwide warehouse 1<sup>st</sup>, or wait a month. All other news sources are saying, HALT, I’m telling you the truth, DELAYED.
+
+Yesterdays news, is today(s) OLD news.
+
+[caption id="attachment_251" align="aligncenter" width="1169"]<img class="size-full wp-image-251" src="https://bridgenewz.com/wp-content/uploads/2025/02/usps-2025-02-05-08-47-04.webp" alt="usps 2025-02-05 08-47-04" width="1169" height="478" /> usps 2025-02-05 08-47-04, Quoted from usps.com[/caption]
+
+&nbsp;
+
+Direct International Mail from China/Temu, Shein will be delay and or held; but still get to you.
+
+Regardless, ALWAYS suspect a delay when shipping international or receiving international mail.
+
+“<a href="https://about.usps.com/newsroom/service-alerts/international">International service disruptions</a>
+<h1 class="western">International inbound mail and packages from China and Hong Kong Posts</h1>
+<strong><span style="color: #ff0000;">UPDATED INFORMATION:</span></strong><span style="color: #ff0000;"> International inbound mail and packages from China and Hong Kong Posts</span>
+
+Effective February 5, 2025, the Postal Service will continue accepting all international inbound mail and packages from China and Hong Kong Posts. The USPS and Customs and Border Protection are working closely together to implement an efficient collection mechanism for the new China tariffs to ensure the least disruption to package delivery.”
+
+https://about.usps.com/newsroom/service-alerts/international/suspension-of-inbound-parcels-from-china-and-hong-kong.htm
+
+Author: Ryan Bridglal, 02/05/2025
