2144f317df98e49ec92c42ed4c284bac434abb22
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2026-08-15 23:40:11 -0700

    Create shop.php

---
+++ holisticharmoni.com/pages/shop.php
@@ -0,0 +1,15 @@
+<!-- initially was the welcome page 
+name changed to shop -->
+
+<?php
+<p style="text-align: center;">All things with an approach to health that considers the whole person, including physical, mental, emotional, social, and spiritual aspects. It emphasizes the interconnected-ness of these dimensions to achieve overall well-being.</p>
+&nbsp;
+
+<img class="wp-image-106 aligncenter" src="https://holisticharmoni.com/wp-content/uploads/2025/12/yoga-5281457_1920.jpg" alt="test1 " width="584" height="389" />
+
+&nbsp;
+<h3 style="text-align: center;"><strong>Featured Sets</strong></h3>
+&nbsp;
+<p style="text-align: center;">[product_category category="Featured" limit="3" columns="3"]</p>
+&nbsp;
+<h3 style="text-align: center;"><strong>Products</strong></h3>
