94ba6d5f72bd5d08018d58aea684e1e061886ea9
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2026-08-23 22:06:01 -0700

    Create footer.php

---
+++ toadspens.com/wp-content-themes-storefront/footer.php
@@ -0,0 +1,15 @@
+        <div style="position: absolute; left: 0px; top: 0px; width: 1300px;
+            height: 500px; overflow: hidden;">
+            <div>
+                <img u="image" src="https://toadspens.com/wp-content/uploads/2026/08/footer3.png" />
+                               <div style="position: absolute; width: 480px; height: 120px; top: 30px; left: 30px; padding: 5px;
+                    text-align: left; line-height: 60px; text-transform: uppercase; font-size: 50px;
+                        color: #FFFFFF;">Welcome
+                </div>
+                <div style="position: absolute; width: 480px; height: 120px; top: 300px; left: 30px; padding: 5px;
+                    text-align: left; line-height: 36px; font-size: 30px;
+                        color: #FFFFFF;">Overall well being.
+                </div>
+            </div>
+          
+         </div>
