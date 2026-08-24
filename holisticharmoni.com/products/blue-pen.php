34864e8fb17213f772d3a6759b38f8e81c898a08
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2026-08-23 22:18:00 -0700

    Create blue-pen.php

---
+++ holisticharmoni.com/products/blue-pen.php
@@ -0,0 +1,17 @@
+ https://holisticharmoni.com/product/blue-v2-reusable-injection-pen/
+
+Blue Reusable Injection Pen
+
+Brand,                   Holistic Harmoni
+Body Material,   Aluminum
+Size CM,               16x1.86
+Size IN,                 6.29x.73
+Type,                     Injector - Peptide Pen
+Use,                       Reusable
+Contains,            Start Button, Adjustable Dial Gauge, Hand Grasp/Handle
+Note,                     This is the main pen body only, cartridge and needle required; sold separately 
+
+
+With Precision-Controlled Delivery, you can fine tune each injection with an adjustable dial for 1 to 60 units per release; consistent and controlled administration.  
+Built for long-term use with high-quality metal components that resist wear and provide a professional grade feel. 
+Universal Cartridge Compatibility, Fits most standard 3 mL long stopper cartridges  
