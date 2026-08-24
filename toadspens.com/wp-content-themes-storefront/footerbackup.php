73256696313e15aa53218cc2e555e1dbcb4ada60
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2026-08-23 22:06:24 -0700

    Create footerbackup.php

---
+++ toadspens.com/wp-content-themes-storefront/footerbackup.php
@@ -0,0 +1,31 @@
+About Us - page
+https://toadspens.com/about-us/?customize_changeset_uuid=e0e68667-b0a7-4e14-a200-8b88c0a9fef4&customize_messenger_channel=preview-0
+
+
+Terms and Conditions - page
+https://toadspens.com/terms-and-conditions/?customize_changeset_uuid=e0e68667-b0a7-4e14-a200-8b88c0a9fef4&customize_messenger_channel=preview-0
+
+
+Frequently Asked Questions | Toads Pens
+https://toadspens.com/faq/?customize_changeset_uuid=e0e68667-b0a7-4e14-a200-8b88c0a9fef4&customize_messenger_channel=preview-0
+
+
+Disclaimer
+https://toadspens.com/terms-and-conditions/#disclaimer
+
+
+Shipping & Returns Policy
+https://toadspens.com/terms-and-conditions/#shipping
+
+
+Privacy Policy
+https://toadspens.com/terms-and-conditions/#privacy
+
+
+
+Contact Toads Pens | Customer Support & Newsletter - page
+https://toadspens.com/contact/?customize_changeset_uuid=e0e68667-b0a7-4e14-a200-8b88c0a9fef4&customize_messenger_channel=preview-0
+
+
+Join our affiliate program - page
+https://toadspens.com/affiliates/?customize_changeset_uuid=e0e68667-b0a7-4e14-a200-8b88c0a9fef4&customize_messenger_channel=preview-0
