7bddbd575a868170218d883b2534026214ac57a2
R. Bridge <7401290+srbridgecom@users.noreply.github.com>
2026-08-23 22:27:23 -0700

    Create Midnight Magic Collection Injectin Pen Set.php

---
+++ holisticharmoni.com/products/pen sets/Midnight Magic Collection Injectin Pen Set.php
@@ -0,0 +1,24 @@
+Midnight Magic Collection Injection Pen Set
+
+Brand,                   Holistic Harmoni
+Body Material,   Aluminum
+Size CM,               16x1.86
+Size IN,                 6.29x.73
+Type,                     Injector - Peptide Pen
+Use,                       Reusable
+Contains,            Start Button, Adjustable Dial Gauge, Hand Grasp/Handle
+Note,                     This is the main pen body only, cartridge and needle required; sold separately 
+
+
+
+
+#Adjustable, #black, #blue, #pink, #silver, #injection, #injectorpen, #medical, #needle, #patientcare, #pens, #peptide, #reusebale, #v2, #sets, #midnightmagic, #magic
+
+
+
+PRECISION & CONTROL Designed with a 60-unit dosing dial and clear viewing window for smooth, controlled adjustments and consistent use..
+REUSABLE & DURABLE DESIGN Crafted with a metallic anodized aluminum body for long-lasting performance and a premium feel—built for repeated use.
+COMPATIBILITY NOTICE Compatible with 3ml cartridges featuring an 11mm long stopper. If using prefilled cartridges, please confirm size prior to ordering.
+Designed with attention to detail and quality standards for customers seeking reliable, reusable injection accessories.
+COMPACT & TRAVEL-FRIENDLY Slim, lightweight design fits easily into a pocket, bag, or case—ideal for home or travel use.
+INCLUDES 4 EMPTY CARTRIDGES Comes with 2 empty 3ml cartridges for convenience and easy setup right out of the box.
